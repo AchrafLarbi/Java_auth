77e8e5287b1aa2e39573c4dcc0392acb33af5089
AchrafLarbi <larbiachraf03@gmail.com>
2024-03-02 20:22:00 +0100

    'java_auth'

---
+++ src/main/java/net/codejava/AppController.java
@@ -0,0 +1,57 @@
+package net.codejava;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AppController {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@GetMapping("")
+	public String viewHomePage() {
+		return "index";
+	}
+
+	@GetMapping("/signin")
+	public String viewLoginPage() {
+		return "signin2";
+	}
+
+	
+	
+	@GetMapping("/register")
+	public String showRegistrationForm(Model model) {
+		model.addAttribute("user", new User());
+		
+		return "signup2";
+	}
+	
+	@PostMapping("/process_register")
+	public String processRegister(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+		
+		userRepo.save(user);
+		
+		return "signin2";
+	}
+	
+	@GetMapping("/users")
+	public String listUsers(Model model) {
+		List<User> listUsers = userRepo.findAll();
+		model.addAttribute("listUsers", listUsers);
+		
+		return "users";
+	}
+}
